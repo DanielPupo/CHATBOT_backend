0ddb10731f4863672952e0038545c6a6b0789dd3
DanielPupo <daniel.pupo.senai@gmail.com>
2026-06-02 14:05:18 -0300

    gemini 3.1 flash lite

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 load_dotenv()
 
 # Define qual versão da IA vamos usar. O modelo "flash" é rápido e ideal para chatbots.
-MODELO = "gemini-3.1-flash"
+MODELO = "gemini-3.1-flash-lite"
 
 # Aqui definimos o "Prompt de Sistema". É a personalidade e as regras que o bot deve seguir.
 instrucoes = """
